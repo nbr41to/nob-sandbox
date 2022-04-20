5f72e1465ed48a8957e863f4403b0bf62cf897cb
Knob <59198478+nbr41to@users.noreply.github.com>
2022-04-20 19:23:41 +0900

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Layout } from '../src/components/Layout';
+import { ReactNode } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
